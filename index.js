7c04e405c17b7663d413d9761f53c6278e6e2373
Steven <s.spencerletts@gmail.com>
2023-06-02 14:59:26 +0100

    user schema setupt

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 
 const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGODB_URI).then(() => {
+        console.log("Connected to mongoDB")
+    }).catch((error) => {
+        throw error
+    })
+}
 
 app.listen(3000, () => {
+    connect()
     console.log("listening on port 3000")
 })
